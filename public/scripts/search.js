14ba144efa7e38115fce3d3f967b9e78d4cca657
cutesd <cutesd@hotmail.com>
2017-09-19 00:38:28 +0000

    refactor search code

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -6,7 +6,18 @@ $('#campground-search').on('input', function() {
     $.get('/campgrounds?' + search, function(data) {
         $('#campground-grid').html('');
         data.forEach(function(camp) {
-            $('#campground-grid').append(' <div class="col-md-3 col-sm-6"> <div class="thumbnail"> <img src="${ camp.image }"> <div class="caption"> <h4>${ camp.name }></h4> </div> <p> <a href="/campgrounds/${ camp._id }" class="btn btn-primary">More Info</a> </p> </div> </div>');
+            $('#campground-grid').append(`
+            <div class="col-md-3 col-sm-6"> 
+                <div class="thumbnail"> 
+                    <img src="${ camp.image }"> 
+                    <div class="caption"> 
+                        <h4>${ camp.name }</h4> 
+                    </div> 
+                    <p> <a href="/campgrounds/${ camp._id }" class="btn btn-primary">More Info</a> 
+                    </p> 
+                </div> 
+            </div>
+            `);
         });
     });
 });
